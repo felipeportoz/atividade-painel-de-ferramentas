12a0d74ce217ce9815ece6d0026a0d5f693b8126
felipeportoz <felipesantosporto@outlook.com>
2026-06-09 20:32:21 -0300

    lógica conversão de moedas

--- js/script.js
+++ js/script.js
@@ -29,3 +29,65 @@ temaBtn.addEventListener("click", () => {
 
 });
 
+const btnConverter = document.getElementById("btnConverter");
+
+btnConverter.addEventListener("click", pegarCotacao);
+
+
+async function pegarCotacao() {
+
+    const valorDigitado =
+        Number(document.getElementById("valorMoeda").value);
+
+    const tipoConversao =
+        document.getElementById("tipoConversao").value;
+
+    const resultado =
+        document.getElementById("resultadoMoeda");
+
+    const cotacaoTexto =
+        document.getElementById("cotacaoAtual");
+
+    try {
+
+        const resposta = await fetch(
+            "https://economia.awesomeapi.com.br/json/last/USD-BRL"
+        );
+
+        const dados = await resposta.json();
+
+        const dolarHoje =
+            Number(dados.USDBRL.bid);
+
+        cotacaoTexto.textContent =
+            `Cotação atual: R$ ${dolarHoje.toFixed(2)}`;
+
+        let valorConvertido;
+
+        if (tipoConversao === "usd") {
+
+            valorConvertido =
+                valorDigitado * dolarHoje;
+
+            resultado.textContent =
+                `${valorDigitado} USD = R$ ${valorConvertido.toFixed(2)}`;
+
+        } else {
+
+            valorConvertido =
+                valorDigitado / dolarHoje;
+
+            resultado.textContent =
+                `R$ ${valorDigitado} = ${valorConvertido.toFixed(2)} USD`;
+
+        }
+
+    } catch (erro) {
+
+        resultado.textContent =
+            "Não foi possível buscar a cotação.";
+
+    }
+
+}
+
